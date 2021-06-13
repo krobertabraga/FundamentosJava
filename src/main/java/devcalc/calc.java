1cd2a3f87821f9344dd17ba6855b722eb6e7cf90
Karina <krobertabraga@gmail.com>
2021-06-13 20:05:52 +0000

    Curso Java

---
+++ src/main/java/devcalc/calc.java
@@ -0,0 +1,15 @@
+// 1 - Pacote
+package devcalc;
+// 2 - Bibliotecas
+
+// 3 Classe
+public class calc {
+    // 3.1 - Atributos
+
+    // 3.2 - Métodos e Funções
+    public static void main(String[] args){
+        System.out.println("Passou pelo método main");
+
+    }
+    public static void somarDoisNumeros() {System.out.println("Passou pelo metodo somarDoisNumeros");}
+}
